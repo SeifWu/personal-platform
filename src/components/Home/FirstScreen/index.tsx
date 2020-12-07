64d4104f3d7df00633ecf6d91421bdd48204131a
Seif Wu <seif.wuxf@gmail.com>
2020-12-07 16:38:55 +0800

    feat: 添加 mask，以及禁止拖拽

--- src/components/Home/FirstScreen/index.tsx
+++ src/components/Home/FirstScreen/index.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
-import VideoBackground from '../../../components/VideoBackground';
+import { makeStyles } from '@material-ui/core/styles'
+
+import VideoBackground from '../../VideoBackground';
+import Mask from '../../Mask';
+
+const useStyles = makeStyles({
+  headerImageBottom: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    bottom: -2,
+    width: '100%',
+    height: 'auto',
+    background: 'transparent',
+    '-webkit-user-drag': 'none',
+  }
+});
 
 const FirstScreen: React.FC<any> = ({ source, backgroundCover }) => {
+  const cls = useStyles();
   return (
     <React.Fragment>
-      <div style={{ position: 'relative', minHeight: '100vh', width: '100%', }}>
+      <div style={{ position: 'relative', minHeight: '100vh', width: '100%', userSelect: 'none', }}>
         <VideoBackground source={source} backgroundCover={backgroundCover} />
-        <img src='/waves.svg'
-          style={{
-            position: "absolute",
-            left: 0,
-            right: 0,
-            bottom: -2,
-            width: '100%',
-            height: 'auto',
-          }}
-        />
+        <Mask />
+        <img src='/waves.svg' className={cls.headerImageBottom} />
       </div>
 
       <div style={{ minHeight: '100vh', width: '100%' }}></div>
--- src/components/Mask/index.tsx
+++ src/components/Mask/index.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  mask: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    width: '100%',
+    height: '100vh',
+  }
+});
 
 const Mask: React.FC<{}> = () => {
+  const cls = useStyles();
   return (
-    <div />
+    <div className={cls.mask} />
   )
 }
 
