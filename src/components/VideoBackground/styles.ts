0475ddcfa5e00d2eeba43a6810dae163d28b5b82
Seif Wu <seif.wuxf@gmail.com>
2020-12-07 14:41:35 +0800

    style: 采用另一种方式居中显示

--- src/components/VideoBackground/styles.ts
+++ src/components/VideoBackground/styles.ts
@@ -3,12 +3,15 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles({
   container: {
     position: 'fixed',
-    minWidth: '100%',
-    minHeight: '100%',
-    right: 0,
-    left: '50%',
-    bottom: 0,
+    // minWidth: '100%',
+    // minHeight: '100%',
+    // right: 0,
+    // left: '50%',
+    // bottom: 0,
+    // transform: 'translate(-50%, 0)',
     zIndex: -1,
-    transform: 'translate(-50%, 0)',
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover',
   }
 })
