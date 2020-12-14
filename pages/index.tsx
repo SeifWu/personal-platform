6b86c31069f073ddb468f2268ee19808b78a2016
Seif Wu <seif.wuxf@gmail.com>
2020-12-14 14:22:35 +0800

    feat: home page optimize

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+
 // import { getStaticProps } from 'next';
 
+import { MediaCard, SectionTitle } from '@/components/Nico';
 import FirstScreen from '../src/components/Home/FirstScreen';
+import { Grid, Container } from '@material-ui/core';
 // import { MenuItem } from '../src/components/Navbar/data';
 // import TopMenu from '../src/components/TopMenu';
 
 export async function getStaticProps() {
-  const res = await fetch('https://seifwu.github.io/mock/api/v1/home.json')
-  const ResponseJson = await res.json()
-  console.log(ResponseJson)
+  const res = await fetch('https://seifwu.github.io/mock/api/v1/home.json');
+  const ResponseJson = await res.json();
+  console.log(ResponseJson);
 
   return {
     props: {
@@ -18,16 +21,27 @@ export async function getStaticProps() {
     // - When a request comes in
     // - At most once every second
     revalidate: 1, // In seconds
-  }
+  };
 }
 
-
-export default function Index({ title = '', background = '', background_cover = '' }) {
-  console.log(title)
+export default function Index({ background = '', background_cover = '' }) {
   return (
     <>
       {/* <TopMenu menuData={menuData} title="Annie" logoUrl="/logo.png" /> */}
       <FirstScreen source={background} backgroundCover={background_cover} />
+      <SectionTitle title="Project" subTitle="个人实践" />
+      <Container>
+        <Grid spacing={3}>
+          <Grid item xs={12} md={4}>
+            <MediaCard
+              coverImg="https://seifwu.github.io/static/img/video-background-1.png"
+              title="IM系统"
+            >
+              这是一段内容
+            </MediaCard>
+          </Grid>
+        </Grid>
+      </Container>
     </>
   );
 }
